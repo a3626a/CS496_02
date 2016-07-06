a6557836fe78d506980d2603a90b4664f937d7f8
hjenie <hjenie06@gmail.com>
2016-07-06 15:54:18 +0900

    errorerrorerror

--- app/src/main/java/kaist/cs496_02/TabAFragment.java
+++ app/src/main/java/kaist/cs496_02/TabAFragment.java
@@ -4,6 +4,8 @@ import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -33,7 +35,6 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -60,82 +61,100 @@ public class TabAFragment extends Fragment {
     CallbackManager callbackManager;
     static JSONAdapter adapter;
     private final String filename = "phonebook";
-    JSONArray jsonArray = new JSONArray();
+    JSONArray jsonArray = new JSONArray(); //for Contacts
+    JSONArray DBjarr2; //for DB
+    JSONArray FBjarr;
+    ArrayList<PhonePerson> values = new ArrayList<>();  //for memory
+    String username="LeeChangHwan";
+
+
+
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        //---------------source from jisu start---------------
-        Dialog dialog = new Dialog(getContext());
-        Uri uri = ContactsContract.Contacts.CONTENT_URI;
+        ConnectivityManager connMgr = (ConnectivityManager) getActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected()) {
+            Log.i("LogCat", "[GET]NETWORK AVAILABLE");
 
-        Cursor cursor = dialog.getContext().getContentResolver().query(uri, null, null, null, null);
-        if (cursor.getCount() > 0) {
-            while (cursor.moveToNext()) {
-                String id = cursor.getString(
-                        cursor.getColumnIndex(ContactsContract.Contacts._ID));
-                String name = cursor.getString(cursor.getColumnIndex(
-                        ContactsContract.Contacts.DISPLAY_NAME));
+        } else {
+            Log.i("LogCat", "[GET]NETWORK ERROR");
+            // fetch data from disk and phonebook
 
-                if (cursor.getInt(cursor.getColumnIndex(
-                        ContactsContract.Contacts.HAS_PHONE_NUMBER)) > 0) {
-                    Cursor pCur = dialog.getContext().getContentResolver().query(
-                            ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
-                            null,
-                            ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?",
-                            new String[]{id}, null);
-                    while (pCur.moveToNext()) {
-                        String phoneNo = pCur.getString(pCur.getColumnIndex(
-                                ContactsContract.CommonDataKinds.Phone.NUMBER));
+            //fetch from phonebook
+            //---------------source from jisu start---------------
 
-                        try {
-                            JSONObject jsonObject = new JSONObject();
-                            jsonObject.put("name", name);
-                            jsonObject.put("number", phoneNo);
-                            jsonArray.put(jsonObject);
-                        } catch (JSONException e) {
-                            throw new RuntimeException(e);
+            Dialog dialog = new Dialog(getContext());
+            Uri uri = ContactsContract.Contacts.CONTENT_URI;
+
+            Cursor cursor = dialog.getContext().getContentResolver().query(uri, null, null, null, null);
+            if (cursor.getCount() > 0) {
+                while (cursor.moveToNext()) {
+                    String id = cursor.getString(
+                            cursor.getColumnIndex(ContactsContract.Contacts._ID));
+                    String name = cursor.getString(cursor.getColumnIndex(
+                            ContactsContract.Contacts.DISPLAY_NAME));
+
+                    if (cursor.getInt(cursor.getColumnIndex(
+                            ContactsContract.Contacts.HAS_PHONE_NUMBER)) > 0) {
+                        Cursor pCur = dialog.getContext().getContentResolver().query(
+                                ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                                null,
+                                ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?",
+                                new String[]{id}, null);
+                        while (pCur.moveToNext()) {
+                            String phoneNo = pCur.getString(pCur.getColumnIndex(
+                                    ContactsContract.CommonDataKinds.Phone.NUMBER));
+
+                            try {
+                                JSONObject jsonObject = new JSONObject();
+                                jsonObject.put("name", name);
+                                jsonObject.put("number", phoneNo);
+                                jsonArray.put(jsonObject);
+                            } catch (JSONException e) {
+                                throw new RuntimeException(e);
+                            }
                         }
+                        pCur.close();
                     }
-                    pCur.close();
                 }
             }
-        }
 
-//-----------------------------source from jisu end---------------------
+            //-----------------------------source from jisu end---------------------
 
-
-        //----------source from changhwan start--------------
-        try {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(getActivity().openFileInput(filename)));
-            String text;
-            String jsonfile="";
-            while ((text=reader.readLine())!=null) {
-                jsonfile+=text;
-            }
-            JSONArray jarray = new JSONArray(jsonfile);
-            ArrayList<PhonePerson> values = new ArrayList<>();
-            for (int i = 0 ; i < jarray.length(); i++) {
-                values.add(new PhonePerson((JSONObject) jarray.get(i)));
-            }
-            adapter = new JSONAdapter(getActivity(), values);
-
-            reader.close();
-        } catch (FileNotFoundException e) {
-            File fPhoneBook = new File(getContext().getFilesDir(), filename);
-            ArrayList<PhonePerson> values = new ArrayList<>();
-            adapter = new JSONAdapter(getActivity(), values);
-        } catch (IOException e) {}
-        catch (JSONException e) {}
-        //----------source from changhwan end------------
-
-        for(int i=0;i<jsonArray.length();i++){
-            JSONObject jobj = null;
+            //fetch from disk
+            //----------source from changhwan start--------------
             try {
-                jobj = jsonArray.getJSONObject(i);
-                TabAFragment.adapter.add(new PhonePerson(jobj));
-            } catch (JSONException e) {}
+                BufferedReader reader = new BufferedReader(new InputStreamReader(getActivity().openFileInput(filename)));
+                String text;
+                String jsonfile="";
+                while ((text=reader.readLine())!=null) {
+                    jsonfile+=text;
+                }
+                JSONArray jarray = new JSONArray(jsonfile);
+                for (int i = 0 ; i < jarray.length(); i++) {
+                    values.add(new PhonePerson((JSONObject)jarray.get(i)));
+                }
+                adapter = new JSONAdapter(getActivity(), values);
+                reader.close();
+            } catch (FileNotFoundException e) {
+                ArrayList<PhonePerson> values = new ArrayList<>();
+                adapter = new JSONAdapter(getActivity(), values);
+            } catch (IOException e) {}
+            catch (JSONException e) {}
+            //----------source from changhwan end------------
+
+
+            for(int i=0;i<jsonArray.length();i++){
+                JSONObject jobj = null;
+                try {
+                    jobj = jsonArray.getJSONObject(i);
+                    TabAFragment.adapter.add(new PhonePerson(jobj));
+                } catch (JSONException e){}
+            }
+
         }
 
     }
@@ -172,13 +191,12 @@ public class TabAFragment extends Fragment {
         //----------------------Start Facebook Part--------------------------------------------------------------------------------
         callbackManager = CallbackManager.Factory.create();
         LoginButton loginButton = (LoginButton) v.findViewById(R.id.login_button);
-        loginButton.setReadPermissions(Arrays.asList("user_friends","public_profile","email")); //access additional profile or post contents
+        loginButton.setReadPermissions(Arrays.asList("user_friends","public_profile")); //access additional profile or post contents
         loginButton.setFragment(this);
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
                 AccessToken accessToken = loginResult.getAccessToken();
-
 
                 GraphRequest request = new GraphRequest(
                         accessToken,
@@ -197,9 +215,14 @@ public class TabAFragment extends Fragment {
                                             if(jsonArray != null){
                                                 for(int i=0;i<jsonArray.length();i++){
                                                     String name = jsonArray.getJSONObject(i).getString("name");
-                                                    String id = jsonArray.getJSONObject(i).getString("id");
-                                                    new SendMSGTask().execute(name, id);
+                                                    String number = "010-" + random4digit() + "-" + random4digit();
+                                                    JSONObject FBobj = new JSONObject();
+                                                    FBobj.put("name", name);
+                                                    FBobj.put("number", number);
+                                                    FBjarr.put(FBobj);
                                                 }
+
+
                                             }
                                         } catch (JSONException e){
                                             e.printStackTrace();
@@ -211,7 +234,24 @@ public class TabAFragment extends Fragment {
                 );
                 request.executeAsync();
 
-
+                /*
+                GraphRequest request2 = GraphRequest.newMeRequest(
+                        accessToken,
+                        new GraphRequest.GraphJSONObjectCallback() {
+                            @Override
+                            public void onCompleted(
+                                    JSONObject object,
+                                    GraphResponse response) {
+                                Log.i("facebookuser",object.toString());
+                            }
+                        });
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "id,name,link");
+                request2.setParameters(parameters);
+                request2.executeAsync();
+                */
+                // fetch data from DB
+                new GetMSGTask().execute(username);
                 viewText.setText("login maintained");
             }
 
@@ -225,12 +265,49 @@ public class TabAFragment extends Fragment {
                 viewText.setText("login error");
             }
         });
-//--------------End Facebook Part---------------------------------------------------------
+    //--------------End Facebook Part---------------------------------------------------------
 
 
         return v;
     }
 
+
+    private String random4digit() {
+        int num = (int) (Math.random() * 10000);
+        if (num < 10) {
+            return "000" + Integer.toString(num);
+        } else if (num < 100) {
+            return "00" + Integer.toString(num);
+        } else if (num < 1000) {
+            return "0" + Integer.toString(num);
+        } else {
+            return Integer.toString(num);
+        }
+    }
+
+    //put DBdata into adapter
+    public void jsonParserList(String src){
+        try{
+            JSONArray DBjarr = new JSONArray(src);
+            JSONObject DBjobj;
+            for(int i=0;i<DBjarr.length();i++){
+                DBjobj = DBjarr.getJSONObject(i);
+                if(DBjobj != null){
+                    String name = DBjobj.getString("name");
+                    String number = DBjobj.getString("number");
+                    JSONObject DBjobj2 = new JSONObject();
+                    DBjobj2.put("name", name);
+                    DBjobj2.put("number",number);
+                    DBjarr2.put(DBjobj2);
+                    values.add(new PhonePerson(DBjobj2));
+                }
+            }
+            adapter = new JSONAdapter(getActivity(), values);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -252,14 +329,12 @@ public class TabAFragment extends Fragment {
         protected String doInBackground(String... params) {
 
             String name = params[0];
-            String id = params[1];
+            String number = params[1];
 
             try {
                 JSONObject obj = new JSONObject();
                 obj.put("name", name);
-                obj.put("number", "010-" + random4digit() + "-" + random4digit());
-                TabAFragment.adapter.add(new PhonePerson(obj));
-                obj.put("id", id);
+                obj.put("number", number);
                 return putJSON(obj);
             } catch (JSONException e) {
             }
@@ -267,18 +342,6 @@ public class TabAFragment extends Fragment {
             return null;
         }
 
-        private String random4digit() {
-            int num = (int) (Math.random() * 10000);
-            if (num < 10) {
-                return "000" + Integer.toString(num);
-            } else if (num < 100) {
-                return "00" + Integer.toString(num);
-            } else if (num < 1000) {
-                return "0" + Integer.toString(num);
-            } else {
-                return Integer.toString(num);
-            }
-        }
 
         @Override
         protected void onPostExecute(String s) {
@@ -347,7 +410,8 @@ public class TabAFragment extends Fragment {
         @Override
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
-            viewText.setText(s);
+            Log.i("ChangHwan", s);
+            jsonParserList(s);
         }
 
         public JSONArray getJSON(String name) {
@@ -356,6 +420,7 @@ public class TabAFragment extends Fragment {
             try {
                 rawURL = server_url + "/" + URLEncoder.encode(name, "UTF-8");
             } catch (UnsupportedEncodingException e) {
+                Log.i("PLACE","getJSON");
                 e.printStackTrace();
             }
             InputStream is = null;
